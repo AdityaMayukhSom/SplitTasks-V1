6963c0baa301f708c4fd4ddc993cd43cc668c06b
Aditya Mayukh Som <adi.kg2@gmail.com>
2025-11-05 00:16:54 +0530

    ST-HYD-0005: fixed duplicate email error test

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,9 +1,11 @@
-from typing import Annotated
+from typing import Annotated, Literal
 
 from fastapi import APIRouter, status, Body
-from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import HTTPException
 from fastapi.responses import JSONResponse
 from pwdlib import PasswordHash
+from pydantic import Field
+from sqlalchemy.exc import IntegrityError
 
 from app.repository.models import User
 from app.routes.base_payload import BasePayload
@@ -15,7 +17,7 @@ user_router = APIRouter()
 class UserRegister(BasePayload):
     email: str
     full_name: str
-    password: str
+    password: str = Field(min_length=8)
     mobile_number: str | None = None
 
 
@@ -24,17 +26,36 @@ class UserIdentifier(BasePayload):
     email: str
 
 
+class UserExistsError(BasePayload):
+    error: Literal["duplicate_email"]
+    error_description: str
+
+
 @user_router.post("/register", response_class=JSONResponse, response_model=UserIdentifier, tags=["user"])
 async def register_user(user_register: Annotated[UserRegister, Body()], session: SessionDep):
     hasher = PasswordHash.recommended()
     hashed_password = hasher.hash(user_register.password)
+
     db_user = User(
         full_name=user_register.full_name,
         email=user_register.email,
         mobile_number=user_register.mobile_number,
         hashed_password=hashed_password,
     )
-    session.add(db_user)
-    session.commit()
+
+    try:
+        session.add(db_user)
+        session.commit()
+    except IntegrityError:
+        err_data = UserExistsError(
+            error="duplicate_email",
+            error_description="account with given email already exists",
+        )
+
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=err_data.model_dump(),
+        )
+
     payload = UserIdentifier(id=db_user.id, email=db_user.email)
-    return JSONResponse(content=jsonable_encoder(payload), status_code=status.HTTP_201_CREATED)
+    return JSONResponse(content=payload.model_dump(), status_code=status.HTTP_201_CREATED)
--- tests/test_user.py
+++ tests/test_user.py
@@ -1,6 +1,5 @@
 import pytest
 from fastapi import status
-from fastapi.encoders import jsonable_encoder
 from fastapi.testclient import TestClient
 from pwdlib import PasswordHash
 from sqlmodel import Session
@@ -8,13 +7,16 @@ from sqlmodel import Session
 from app.repository.models import User
 from app.routes.user import UserRegister
 
-testdata = [("Otto Octavius", "otto.octavius@gmail.com", "spiderman")]
+testdata = [
+    ("Albert Einstein", "albert.e@princeton.edu", "PhotoElectric@1905"),
+    ("Walter White", "heisenberg@abqchem.com", "SayMyName!"),
+]
 
 
 @pytest.mark.parametrize(["name", "email", "password"], testdata)
 def test_user_creation(name: str, email: str, password: str, client: TestClient, session: Session):
     user_register = UserRegister(email=email, full_name=name, password=password)
-    resp = client.post("/user/register", json=jsonable_encoder(user_register))
+    resp = client.post("/user/register", json=user_register.model_dump())
 
     assert resp.status_code == status.HTTP_201_CREATED
     data = resp.json()
@@ -29,3 +31,19 @@ def test_user_creation(name: str, email: str, password: str, client: TestClient,
 
     hasher = PasswordHash.recommended()
     assert hasher.verify(user_register.password, user_db.hashed_password)
+
+
+@pytest.mark.parametrize(["name", "email", "password"], testdata)
+def test_duplicate_user_via_api(name: str, email: str, password: str, client: TestClient, session: Session):
+    user_register = UserRegister(email=email, full_name=name, password=password)
+    payload = user_register.model_dump()
+
+    resp_suc = client.post("/user/register", json=payload)
+    assert resp_suc.status_code == status.HTTP_201_CREATED
+    data_suc = resp_suc.json()
+    assert data_suc["email"] == email
+
+    resp_err = client.post("/user/register", json=payload)
+    assert resp_err.status_code == status.HTTP_409_CONFLICT
+    data_err = resp_err.json()
+    assert data_err["detail"]["error"] == "duplicate_email"
